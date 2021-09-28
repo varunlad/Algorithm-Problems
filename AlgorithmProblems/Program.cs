ee08dfc39c82b905cc773e6824102ac7d8140ed0
varun <varunlad5@gmail.com>
2021-09-28 20:10:09 +0530

    [varun] Bubble Sorting in a List

---
+++ AlgorithmProblems/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlgorithmProblems
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> list = new List<int>();
+
+            list.Add(1);
+            list.Add(5);
+            list.Add(7);
+            list.Add(3);
+            list.Add(17);
+            list.Add(10);
+            list.Add(13);
+            list.Add(9);
+            Console.WriteLine("Unsorted List");
+            list.ForEach(Console.WriteLine);
+            Sorting obj = new Sorting();
+            obj.Sort(list);
+            Console.WriteLine("\nSorted List");
+            list.ForEach(Console.WriteLine);
+            Console.ReadLine();
+          
+        }
+    }
+}
---
+++ AlgorithmProblems/Sorting.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlgorithmProblems
+{
+   public class Sorting
+    {
+        public void Sort(List<int> list)
+        {
+            int size = list.Capacity;
+            for (int i = 1; i < size; i++)
+            {
+                for (int j = 0; j < (size - i); j++)
+                {
+                    if (list[j] > list[j + 1])
+                    {
+                        int temp = list[j];
+                        list[j] = list[j + 1];
+                        list[j + 1] = temp;
+                    }
+                }
+            }
+        }
+    }
+}
