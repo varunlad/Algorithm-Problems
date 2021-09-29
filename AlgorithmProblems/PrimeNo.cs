073f92947b06cefd9520ade61bae1c2bf0e2f3e2
varun <varunlad5@gmail.com>
2021-09-29 20:47:46 +0530

    [varun] Prime Number From 1 to 1000

---
+++ AlgorithmProblems/PrimeNo.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlgorithmProblems
+{
+    public class PrimeNumber
+    {
+        public void Prime()
+        {
+            for (int i = 3; i <= 100; i++)//Outer For Loop to increment numbers from 2 till 1000
+            {
+                bool isPrime = true;
+                for (int j = 2; j <=i/2; j++)//We check number till half of the number
+                {
+                    if (i % j == 0)
+                    {
+                        isPrime = false;
+                        break;
+                    }
+                }
+                if (isPrime)
+                {
+                    Console.WriteLine("Prime number-->  "+i);//Printing Prime Number
+                }
+            }
+        }
+    }
+}
+
--- AlgorithmProblems/Program.cs
+++ AlgorithmProblems/Program.cs
@@ -10,22 +10,8 @@ namespace AlgorithmProblems
     {
         static void Main(string[] args)
         {
-            List<int> list = new List<int>();
-            //Adding Elements in a List
-            list.Add(1);
-            list.Add(5);
-            list.Add(7);
-            list.Add(3);
-            list.Add(17);
-            list.Add(10);
-            list.Add(13);
-            list.Add(9);
-            Console.WriteLine("Unsorted List");
-            list.ForEach(Console.WriteLine);//Displaying UnSorted List
-            Sorting obj = new Sorting();
-            obj.BubbleSort(list);
-            Console.WriteLine("\nSorted List");
-            list.ForEach(Console.WriteLine);//Displaying Sorted List
+            PrimeNumber num =new PrimeNumber();
+            num.Prime();//calling method
             Console.ReadLine();
           
         }
