e459f0bba9d0c56ae988dc8a0d7b521e7633d4ea
varun <varunlad5@gmail.com>
2021-10-05 23:52:41 +0530

    [varun] Finding out What number User is Thinking of by itterative method

--- AlgorithmProblems/Program.cs
+++ AlgorithmProblems/Program.cs
@@ -7,43 +7,47 @@ using System.Threading.Tasks;
 namespace AlgorithmProblems
 {
     class Program
-    {
-        static int searching(int[] array, int first, int last, int value)
-        {
-            int middle = (first + last) / 2;//finding Middle
-            if (array[middle] == value)
-            {
-                return middle;
-            }
-            if (first >= last)//checking if the array contain more than one element
-            {
-                return -1;
-            }
-            if (value < array[middle])
-            {
-                return searching(array, first, middle, value);
-            }
-            else
-            {
-                return searching(array, middle, last, value);
-            }
-        }
-
-
-
-        // Driver method to test above
+    { 
         public static void Main()
         {
-            var numbers = new[] { 1,5,0,34,3,9,11,12,13,14,15,16,17,18,19,20,67,27,2,3,4,6,7,8,10 };
-           // Array.Sort(numbers);
-            Console.WriteLine("Think of a number");
-            Console.WriteLine("Enter number you were thinking of ");
-            int num = Convert.ToInt32(Console.ReadLine());//reading a number which you enter
-            var index = searching(numbers,0,numbers.Length-1, num);
-            Console.WriteLine("Number {0} is present at index:-",num);
-            Console.WriteLine(index);
-            Console.ReadLine();
+            var numbers = new[] {0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20 };//Taking an array of 20 numbers
+            bool num = true; ;
+            Console.WriteLine("Think of a number between 1 and 20");     
+            int first = 0;
+            int last = numbers.Length ;
+            int middle = (first + last) / 2;         
+            int digit;
+            while (num)//Loop will Continue till it becomes false
+            {
+                Console.WriteLine("Is your number :" + middle);
+                Console.WriteLine("If not Click 1 if your number is less than : " + middle);
+                Console.WriteLine("If not Click 2 if number is greater than  :" + middle);
+                Console.WriteLine("Yes than Click 3 to exit");
+                 digit = Convert.ToInt32(Console.ReadLine());
+                switch (digit)
+                {
+                    case 1:
+                        first = first;
+                        last = middle - 1;  //Making last Element as last to the Middle Element                     
+                        middle = (first + last) / 2;                     
+                        break;
+                    case 2:
+                        first = middle + 1;//Making First element as next to the Middle Element
+                        last = last;
+                        middle = (first + last) / 2;                     
+                        break;
+                    case 3:
+                        num = false;
+                        break;
+                }
+            }
+                
+                
+
+          
+
         }
+
     }
 }
 
